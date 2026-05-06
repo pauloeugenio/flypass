302023bce8803b70b7efb4e23707ea0383301a42
pauloeugenio <pauloeugenio@hotmail.com.br>
2026-05-06 14:35:32 +0200

    Adicionando os primeiros arquivos

---
+++ tracker.py
@@ -0,0 +1,52 @@
+from fast_flights import FlightData, Passengers, create_filter, get_flights
+from datetime import datetime
+import csv
+import os
+
+ORIGIN = "PAR"
+DESTINATION = "ROM"
+DATE = "2026-08-15"
+DATA_FILE = "data/prices.csv"
+
+os.makedirs("data", exist_ok=True)
+
+filter = create_filter(
+    flight_data=[
+        FlightData(
+            date=DATE,
+            from_airport=ORIGIN,
+            to_airport=DESTINATION
+        )
+    ],
+    trip="one-way",
+    passengers=Passengers(adults=1),
+)
+
+result = get_flights(filter)
+
+exists = os.path.exists(DATA_FILE)
+
+with open(DATA_FILE, "a", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+
+    if not exists:
+        writer.writerow([
+            "timestamp",
+            "origin",
+            "destination",
+            "date",
+            "price",
+            "airline",
+            "duration"
+        ])
+
+    for flight in result.flights:
+        writer.writerow([
+            datetime.now().isoformat(),
+            ORIGIN,
+            DESTINATION,
+            DATE,
+            flight.price,
+            flight.name,
+            flight.duration
+        ])
